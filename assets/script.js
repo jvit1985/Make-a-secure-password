9990c750b1e2606852e3ff50725f0b895debccc9
Justin Vititoe <jvit1985@yahoo.com>
2022-03-14 18:07:43 -0500

    fixed undefined and null issues with password generator

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
     var getPasswordLength = function() {
-        passwordLength = prompt("How many characters do you need for your password? Must be between 8 and 128 characters.");
+        passwordLength = prompt("How many characters would you like for your password to be? Must be between 8 and 128 characters.");
 
         passwordLength = parseInt(passwordLength);
 
@@ -25,7 +25,7 @@ function writePassword() {
         return passwordLength;
         }
         else {
-            window.alert("Select a value between 8 and 128");
+            window.alert("Please select a value between 8 and 128");
             return getPasswordLength();
         }
     };
@@ -34,36 +34,78 @@ function writePassword() {
 //ask if using upper case characters
 var getCharacterSet = function () {
 
-var promptUpperCase = window.prompt ('Would you like to use upper case letters for your password? Type "Y" for "yes" or "N" for "no"');
+    var promptUpperCase = function () {
+        UpperCase = prompt ('Would you like to use upper case letters for your password? Type "y" for "Yes" or "n" for "No"');
 
-console.log(promptUpperCase);
+    if (UpperCase === "Y" || UpperCase === "y") {
+        validCharacters += passwordCharacters.upperCase;
+    }
+    else if (UpperCase === "N" || UpperCase === "n") {
+    }
+    else {
+        window.alert("Please enter y or n.")
+        return promptUpperCase();
+    }
+    };
+    promptUpperCase ();
 
-if (promptUpperCase === "Y" || promptUpperCase === "y") {
-    validCharacters += passwordCharacters.upperCase;
-}
-var promptLowerCase = window.prompt ('Would you like to use lower case letters for your password? Type "Y" for "yes" or "N" for "no"');
+    var promptLowerCase = function () {
+        LowerCase = prompt ('Would you like to use lower case letters for your password? Type "y" for "Yes" or "n" for "No"');
 
-console.log(promptLowerCase);
+    if (LowerCase === "y" || LowerCase === "Y") {
+        validCharacters += passwordCharacters.lowerCase;
+    }
 
-if (promptLowerCase === "y" || promptLowerCase === "Y") {
-    validCharacters += passwordCharacters.lowerCase;
-}
-var promptNumber = window.prompt ('Would you like to use numbers for your password? Type "Y" for "yes" or "N" for "no"');
+    else if (LowerCase === "n" || LowerCase === "N") {
+    }
 
-console.log(promptNumber);
+    else {
+        window.alert("Please enter y or n.")
+        return promptLowerCase();
+    }
+    };
+    promptLowerCase();
 
-if (promptNumber === "y" || promptNumber === "Y") {
-    validCharacters += passwordCharacters.number;
-}
-var promptSymbol = window.prompt ('Would you like to use special characters for your password? Type "Y" for "yes" or "N" for "no"');
+    var promptNumber = function () {
+        NumberCase = prompt ('Would you like to use numbers for your password? Type "y" for "Yes" or "n" for "No"');
 
-console.log(promptSymbol);
-var prompts = [promptUpperCase, promptLowerCase, promptNumber, promptSymbol];
-console.log(prompts);
+    if (NumberCase === "y" || NumberCase === "Y") {
+        validCharacters += passwordCharacters.number;
+    }
 
-if (promptSymbol === "y" || promptSymbol === "Y") {
-    validCharacters += passwordCharacters.symbol;
-}
+    else if (NumberCase === "n" || NumberCase === "N") {
+    }
+
+    else {
+        window.alert("Please enter y or n.")
+        return promptNumber();
+    }
+    };
+    promptNumber();
+
+    var promptSymbol = function () {
+        Symbol = prompt ('Would you like to use special characters for your password? Type "y" for "Yes" or "n" for "No"');
+
+//var prompts = [promptUpperCase, promptLowerCase, promptNumber, promptSymbol];
+
+    if (Symbol === "y" || Symbol === "Y") {
+        validCharacters += passwordCharacters.symbol;
+    }
+
+    else if (Symbol === "n" || Symbol === "N") {
+    }
+
+    else {
+        window.alert("Please enter y or n.")
+        return promptSymbol();
+    }
+    };
+    promptSymbol();
+    
+    if (validCharacters === "") {
+        window.alert ("Please select at least 1 character type!")
+        return getCharacterSet();
+    }
 
 };
 getCharacterSet();
